35c2898e6bef89ce1aa469be946bd8da08edc6cc
Will <will.mendil@gmail.com>
2020-08-01 00:58:02 +0200

    issues when putting devices in specific file

--- include/utils.h
+++ include/utils.h
@@ -1,21 +1,38 @@
 #pragma once
 
+void servoDevice(byte selectedMenuItem);
+
 void todoDevice(byte selectedMenuItem);
 
 void loadDevice(byte selectedMenuItem) ;
 
+
+
 /* Menu principal */
-static const char* MAIN_MENU_ITEMS[] = {"Servo",  "Stepper",  "DC motor",  "Relay",  "Ultrasound",  "Potentiometer",  "Button",   " "};
+static const char* MAIN_MENU_ITEMS[] = {"Servo",  "Stepper",  "DC motor",  "Relay",  "Ultrasound",  "Potentiometer",  "Button", "LED",  " "};
 static int EMPTY_PARAMS[0];
 static Limits_t EMPTY_LIMITS[0];
 
 static Menu_t MAIN_MENU = {
   MAIN_MENU_ITEMS,
-  7,
+  8,
   EMPTY_PARAMS, 
   0,
   EMPTY_LIMITS,
   &loadDevice
+};
+
+static const char* SERVO_MENU_ITEMS[] = {"<-Back | D3",  "Angle: ",  "Delay: ", "w/ rst: ", "MOVE",  " "};
+static int SERVO_PARAMS[] = {180,  5, 1};
+static Limits_t SERVO_LIMITS[] = {{0, 180, 1}, {0, 50, 1}, {0, 1, 1}};
+
+static Menu_t SERVO_MENU = {
+  SERVO_MENU_ITEMS,
+  5,
+  SERVO_PARAMS,
+  3,
+  SERVO_LIMITS,
+  &servoDevice
 };
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-/* Inclut la librairie LiquidCrystal pour le LCD */
 #include "Arduino.h"
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
@@ -6,24 +5,24 @@
 #include "structs.h"
 #include "main.h"
 #include "utils.h"
-#include "servos.h"
  
-/* Objet LCD sur les broches utilisées par la shield LCD DFrobots */
+
 LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
+// Defining special characters for LCD
 #define CURSOR_RIGHT 0x00
 #define CURSOR_UP 0x01
 #define CURSOR_DOWN 0x02
 
+// Defining pins for buttons
 #define BTN_SELECT 11
 #define BTN_UP 12
 #define BTN_DOWN 10
-uint8_t cursorRight[8]  = {0x00,0x08,0x04,0x02,0x04,0x08,0x00,0x00};
-uint8_t cursorUp[8] = {0x00,0x00,0x00,0x00,0x04,0x0E,0x1F,0x00};
-uint8_t cursorDown[8] = {0x00,0x1F,0x0E,0x04,0x00,0x00,0x00,0x00};
 
 
-/** Fonction retournant le bouton appuyé (s’il y en a un). */
+
+// readPushButton reads which button is pressed and returns the enum.
+// If no button is pressed, it returns BP_NONE.
 Button_t readPushButton(void) {
 
   if (digitalRead(BTN_DOWN)){
@@ -39,121 +38,119 @@ Button_t readPushButton(void) {
   
 }
  
-/** Affiche le menu passé en argument */
+// displayMenu prints the menu parsed in argument to the LCD screen.
+// If the menu has parameters, it will print them also.
 void displayMenu(const Menu_t &menu) {
  
-  /* Variable pour le menu */
-  byte selectedMenuItem = 0;   // Choix selectionné
-  Button_t buttonPressed;      // Contient le bouton appuyé
+  byte selectedMenuItem = 0;   // determined which item is selected
+  Button_t buttonPressed;      // contains which but is pressed
  
-  /* Tant que l'utilisateur ne veut pas quitter pas le menu */
+  // Inifite loop whilst the menu is idle
   while(1) {
  
-    /* Affiche le menu */
+    // Prints the different cursors on the screen
     lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.write(CURSOR_RIGHT);
-    if (selectedMenuItem > 0){
+    lcd.write(CURSOR_RIGHT); // prints ">" cursor.
+
+    if (selectedMenuItem > 0){ // prints up cursor on the far right only if not at top
       lcd.setCursor(15,0);
 		  lcd.write(CURSOR_UP);
     }
-    if (selectedMenuItem < menu.nbItems-1){
+    if (selectedMenuItem < menu.nbItems-1){ // prints down cursor on the far right only if not at bottom
       lcd.setCursor(15,1);
 		  lcd.write(CURSOR_DOWN);
     }
 
+    // Prints the nth and nth+1 items from the given menu
     lcd.setCursor(1, 0);
     lcd.print(menu.items[selectedMenuItem]);
     lcd.setCursor(1, 1);
     lcd.print(menu.items[selectedMenuItem+1]);
 
-
+    // Checks if there are parameters
     switch (menu.nbParams) {
-      case 0:
+      case 0: // if not, breaks the switch (case of main menu)
         break;
       default:
-        if (selectedMenuItem == 0 && menu.nbParams > 0){
+        // If there are paramaters, make sure to only print the paramters on the correct lines
+        if (selectedMenuItem == 0){ 
           lcd.setCursor(8, 1);
           lcd.print(menu.params[selectedMenuItem]);
         } 
-        else if (selectedMenuItem > 0 && selectedMenuItem < menu.nbParams && menu.nbParams > 0) {
+        else if (selectedMenuItem > 0 && selectedMenuItem < menu.nbParams) { 
           lcd.setCursor(8, 0);
           lcd.print(menu.params[selectedMenuItem-1]);
           lcd.setCursor(8, 1);
           lcd.print(menu.params[selectedMenuItem]);
         }
-        else if (selectedMenuItem == menu.nbParams &&  menu.nbParams > 0) {
-    
+        else if (selectedMenuItem == menu.nbParams) {     
           lcd.setCursor(8, 0);
           lcd.print(menu.params[selectedMenuItem-1]);
         }
     }
     
  
-    /* Attend le relâchement du bouton */
+    // Check which button pressed (This method allows to activate on release)
     while(readPushButton() != BP_NONE);
- 
-    /* Attend l'appui sur un bouton */
     while((buttonPressed = readPushButton()) == BP_NONE);
- 
-    /* Anti rebond pour le bouton */
-    delay(30);
- 
-    /* Attend le relâchement du bouton */
+    delay(30); // For debouncing
     while(readPushButton() != BP_NONE);
  
-    /* Gére l'appui sur le bouton */
+    // Process which button is pressed
     switch(buttonPressed) {
-    case BP_UP: // Bouton haut = choix précédent
- 
-      /* Si il existe un choix précédent */
+    case BP_UP:
       if(selectedMenuItem > 0) {
- 
-        /* Passe au choix précédent */
-        selectedMenuItem--;
+        selectedMenuItem--; // decrement variable if not at top of list
+      }else { // if reach top, round back to bottom
+        selectedMenuItem = menu.nbItems-1;
       }
       break;
- 
-    case BP_DOWN: // Bouton bas = choix suivant
- 
-      /* Si il existe un choix suivant */
+
+    case BP_DOWN: 
       if(selectedMenuItem < (menu.nbItems - 1)) {
- 
-        /* Passe au choix suivant */
-        selectedMenuItem++;
+        selectedMenuItem++; // increment variable unless at bottom of list
+      } else { // if reach bottom, round back to top
+        selectedMenuItem = 0;
       }
       break;
   
     case BP_SELECT:
-      menu.callbackFnct(selectedMenuItem);
+      menu.callbackFnct(selectedMenuItem); // trigger function from struct 
       break;   
     }
   }
 }
 
  
-/** Setup */
 void setup() {
  
-  // initialize LED digital pin as an output.
+  // initialise pins.
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(BTN_SELECT, INPUT);
   pinMode(BTN_UP, INPUT);
   pinMode(BTN_DOWN, INPUT);
 
-  lcd.init();                      // initialize the lcd 
+  // Creating hex array for each special characters
+  uint8_t cursorRight[8]  = {0x00,0x08,0x04,0x02,0x04,0x08,0x00,0x00};
+  uint8_t cursorUp[8] = {0x00,0x00,0x00,0x00,0x04,0x0E,0x1F,0x00};
+  uint8_t cursorDown[8] = {0x00,0x1F,0x0E,0x04,0x00,0x00,0x00,0x00};
+
+  lcd.init();  // initialize the lcd 
+
+  // Creating special characters
   lcd.createChar(CURSOR_RIGHT, cursorRight);
   lcd.createChar(CURSOR_UP, cursorUp);
   lcd.createChar(CURSOR_DOWN, cursorDown);
-  lcd.backlight();
 
+  //Turning LCD backligth on
+  lcd.backlight();
 
 }
  
-/** Programme principal */
+
 void loop() {
  
-  /* Affiche le menu principal */
-  displayMenu(MAIN_MENU);
+  displayMenu(MAIN_MENU); //This actually launches the whole system by first showing the main menu
  
 }
